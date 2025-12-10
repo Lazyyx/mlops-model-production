c6bdda09f3edfcfce2f533ef2815c6cbf04d3059
Eugenie Beauvillain <eugeniebeauvillain@gmail.com>
2025-12-10 15:44:14 +0100

    FEAT: added limit throught ngnix and not redis

--- src/api/security.py
+++ src/api/security.py
@@ -1,21 +1,8 @@
 import os
-from redis import Redis
 from fastapi import Header, HTTPException, Depends
 
-
-MAX_CALLS = 100
-REDIS_HOST = "redis"
 # for now we store in env file
 API_SECRET_TOKEN = os.environ.get("APP_TOKEN") 
-
-# init redis only once
-try:
-    redis_client = Redis(host=REDIS_HOST, port=6379, decode_responses=True)
-    redis_client.ping()
-    print("Connexion Redis réussie.")
-except Exception as e:
-    print(f"Erreur de connexion Redis: {e}")
-    redis_client = None
 
 async def verify_api_key(x_api_key: str = Header(None, alias="X-API-Key")):
     
@@ -32,24 +19,3 @@ async def verify_api_key(x_api_key: str = Header(None, alias="X-API-Key")):
             status_code=401,
             detail="Accès refusé. Token 'X-API-Key' invalide ou manquant."
         )
-    
-    return x_api_key
-
-async def limit_session_calls(session_id: str = Depends(verify_api_key)):
-    if redis_client is None:
-        raise HTTPException(status_code=503, detail="Le service de session est indisponible.")
-
-    # on utilise la clé session pour faire le compteur
-    key = f"calls:{session_id}" 
-    
-    call_count = redis_client.incr(key)
-
-    if call_count > MAX_CALLS:
-        raise HTTPException(
-            status_code=429,
-            detail=f"Limite de {MAX_CALLS} appels par session dépassée."
-        )
-    
-    # expire après 1 heure (3600 secondes)
-    if call_count == 1:
-        redis_client.expire(key, 3600)
